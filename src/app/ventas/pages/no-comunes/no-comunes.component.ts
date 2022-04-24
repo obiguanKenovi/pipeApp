16f174a31087e2b72fa19ab00ce5912ab5666f99
userjoseg <jrgonzalez@hiberus.com>
2022-04-24 18:31:27 +0200

    Se utiliza el pipe i18nSelect y el i18nPlural

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -16,9 +16,40 @@ export class NoComunesComponent implements OnInit  {
     'femenino':'invitarla'
   };
 
+  //i18nPlural
+  clientes:string[]=['María','Pedro','Manuel','Carlos'];
+  clienteMapa={
+    '=0':'no tenemos ningún cliente esperando.',
+    '=1':'tenemos un cliente esperando',
+    'other':'tenemos # clienes esperando'
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cambiarNombre(){
+
+    if ( this.genero==='femenino')
+    {
+      this.genero='masculino';
+      this.nombre='Jose';
+    }    
+     else
+     {
+      this.genero='femenino';
+      this.nombre='María';
+     }
+  }
+
+  borrarCliente(){ 
+      //Remueve el ultimo elemento de la clase.
+      if(this.clientes.length>0)
+      {
+        this.clientes.pop();
+      }     
   }
 
 }
