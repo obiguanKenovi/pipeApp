ef1a23eb382fe102b4db1979abb64aa8c013e410
userjoseg <jrgonzalez@hiberus.com>
2022-04-24 04:33:57 +0200

    Se cambio el idioma de la fecha revisar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
 import { VentasModule } from './ventas/ventas.module';
+
+//Inicio Cambiar el local de la app
+
+import localeEs from '@angular/common/locales/es-VE';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData( localeEs );
+//Fin cambiar el local de la app o la fecha
 
 
 
@@ -18,7 +26,13 @@ import { VentasModule } from './ventas/ventas.module';
     SharedModule,
     VentasModule 
   ],
-  providers: [],
+  providers: [
+    //Con esta importacion se cambia la fecha de forma global
+    {
+      provide:LOCALE_ID, useValue:'es-VE'
+    }
+    //fin de cambiar el idioma de la fecha de forma global...
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
